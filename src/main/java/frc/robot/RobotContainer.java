3840819439d6fbf3231a61989e16827ea9b67821
Benjamin Clark <benclarkbobb@gmail.com>
2026-02-26 20:31:47 -0500

    Fixed errors about swerve subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,6 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
-import frc.robot.subsystems.swerve.SwerveSubsystem;
 import frc.robot.subsystems.vision.DisenableTrackerCommand;
 import frc.robot.Constants.Gyro;
 import frc.robot.autos.DriveFwd2s;
@@ -48,7 +47,6 @@ public class RobotContainer {
     //private static final ClimberSubsystem climber = new ClimberSubsystem();
     //private static final GyroSubsystem gyro = new GyroSubsystem();
     //private static final IntakeSubsystem intake = new IntakeSubsystem();
-    private static final SwerveSubsystem swerve = new SwerveSubsystem();
     private static final VisionSubsystem vision = new VisionSubsystem();
     private static final TurretSubsystem turret = new TurretSubsystem();
 
@@ -163,7 +161,6 @@ public class RobotContainer {
     //public static ClimberSubsystem getClimberSubsystem() { return climber; }
     //public static GyroSubsystem getGyroSubsystem() { return gyro; }
     //public static IntakeSubsystem getIntakeSubsystem() { return intake; }
-    public static SwerveSubsystem getSwerveSubsystem() { return swerve; }
     public static VisionSubsystem getVisionSubsystem() { return vision; }
     public static TurretSubsystem getTurretSubsystem() { return turret; }
 }
