b05edb14fc2aff3c201ac2d54dc2e3ea553ade9d
JadeCat4 <julian.alexander.cortes@gmail.com>
2026-03-01 12:22:02 -0500

    Shoot Auto
    Added a simple auto that shoots for 4 seconds

---
+++ src/main/java/frc/robot/autos/Shoot.java
@@ -0,0 +1,22 @@
+package frc.robot.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.turret.*;
+import frc.robot.subsystems.turret.ShootCommand;
+
+public class Shoot extends SequentialCommandGroup {
+    
+
+    public Shoot(TurretSubsystem turretSubsystem) {
+        
+
+        addCommands(
+
+
+            new ShootCommand(turretSubsystem).withTimeout(4)
+
+        );
+           
+        
+    }
+}
