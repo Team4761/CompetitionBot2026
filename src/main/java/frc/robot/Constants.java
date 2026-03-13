fa0b471174de179af24b45d4626423a22ca7b87d
kugelblitz150-hash <kugelblitz150@gmail.com>
2026-03-12 20:57:59 -0400

    tweaked some constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -68,8 +68,8 @@ public class Constants {
 
             public static final double KICKER_INIT_DELAY = 1; // Units: seconds
 
-            public static final double TURRET_HORIZONTAL_MULTIPLIER = 2.5;
-            public static final double TURRET_VERTICAL_MULTIPLIER = 1.5;
+            public static final double TURRET_HORIZONTAL_MULTIPLIER = 3.25;
+            public static final double TURRET_VERTICAL_MULTIPLIER = 1.95;
         }
 
         public class Horizontal {
