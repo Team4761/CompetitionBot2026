75b3d9e1cd0768066593b7c9ba8adef9d78871ca
TheManBehindTheMemes <alex@merc-law.com>
2026-03-12 20:59:56 -0400

    DepotAuto conditionals
    added the framework to add three variations of the auto, one for each starting position

--- src/main/java/frc/robot/autos/DepotAuto.java
+++ src/main/java/frc/robot/autos/DepotAuto.java
@@ -16,7 +16,7 @@ import frc.robot.Constants;
 
 public class DepotAuto extends SequentialCommandGroup{
 
-   private static final double AUTO_SPEED_MPS =
+    private static final double AUTO_SPEED_MPS =
         0.35 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
 
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
@@ -24,49 +24,41 @@ public class DepotAuto extends SequentialCommandGroup{
 
     public DepotAuto(IntakeSubsystem intakeSubsystem,CommandSwerveDrivetrain drivetrain){
 
+    }
+
         final var idle = new SwerveRequest.Idle();
-        
-        //theoretically, if distances were arbitrary, this would work perfectly
-        addCommands(
-            new DoNothingCommand(),//as a buffer so that the thing that happened las tyear doesent happen
 
-            drivetrain.runOnce(drivetrain::seedFieldCentric),
+        //theoretically, if distances were arbitrary, this would work fine, assuming we start in the center
+        //IMPORTANT: we need to calcutlate how many seconds it needs to move, and then create variations.
+        //also using a constructor instead of writing the command every time would be nice
+        switch (Constants.Field.STARTING_POSITION){
+            case "CENTER": {
+                addCommands(
+                    new DoNothingCommand(),//as a buffer so that the thing that happened las tyear doesent happen
+                    drivetrain.runOnce(drivetrain::seedFieldCentric),
+                    //right here, we need to add a conditional to determine which starting slot our robot is in
+                    //instead of this, we could possibly check and move to april tag 9 (red) or april tag 25 (blue)
+                    drivetrain.applyRequest(() ->
+                        drive.withVelocityX(AUTO_SPEED_MPS).withVelocityY(0.0).withRotationalRate(0.0)).withTimeout(4.0),//temp value
+                    drivetrain.applyRequest(() -> idle).withTimeout(0.1), //i don't know if this is necessary, but i added a small buffer in between each movement just in case
+                    drivetrain.applyRequest(() -> 
+                        drive.withVelocityX(0.0).withVelocityY(0.0).withRotationalRate(-90.0)).withTimeout(2.0),
+                    drivetrain.applyRequest(() -> idle).withTimeout(0.1),
+                    drivetrain.applyRequest(() ->
+                        drive.withVelocityX(AUTO_SPEED_MPS).withVelocityY(0.0).withRotationalRate(0.0)).withTimeout(6.0),//temp value
+                    drivetrain.applyRequest(() -> idle).withTimeout(0.1),
+                    drivetrain.applyRequest(() -> 
+                        drive.withVelocityX(0.0).withVelocityY(0.0).withRotationalRate(-90.0)).withTimeout(2.0),
+                    drivetrain.applyRequest(() -> idle).withTimeout(0.1),
+                    new ExtendDownMoveAndGather(intakeSubsystem, drivetrain)
+                );
+            }
+            case "LEFT": {
 
-            //right here, we need to add a conditional to determine which starting slot our robot is in
-            //instead of this, we could check and move to april tag 9 (red) or april tag 25 (blue)
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(AUTO_SPEED_MPS)
-                    .withVelocityY(0.0)
-                    .withRotationalRate(0.0)
-            ).withTimeout(4.0),//temp value
+            }
+            case "RIGHT": {
 
-            drivetrain.applyRequest(() -> idle).withTimeout(0.1), //i don't know if this is necessary, but i added a small buffer in between each movement just in case
-
-            drivetrain.applyRequest(() -> 
-                drive.withVelocityX(0.0)
-                    .withVelocityY(0.0)
-                    .withRotationalRate(-90.0)
-            ).withTimeout(2.0),
-
-            drivetrain.applyRequest(() -> idle).withTimeout(0.1),
-
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(AUTO_SPEED_MPS)
-                    .withVelocityY(0.0)
-                    .withRotationalRate(0.0)
-            ).withTimeout(6.0),//temp value
-
-            drivetrain.applyRequest(() -> idle).withTimeout(0.1),
-
-            drivetrain.applyRequest(() -> 
-                drive.withVelocityX(0.0)
-                    .withVelocityY(0.0)
-                    .withRotationalRate(-90.0)
-            ).withTimeout(2.0),
-
-            drivetrain.applyRequest(() -> idle).withTimeout(0.1),
-
-            new ExtendDownMoveAndGather(intakeSubsystem, drivetrain)
-        );
+            }
+        }
     }
 }
