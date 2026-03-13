b388e489b77c6629e7d04ded6599c8afac0b364f
Benjamin Clark <benclarkbobb@gmail.com>
2026-03-12 20:14:11 -0400

    Took out waits

--- src/main/java/frc/robot/subsystems/intake/JostleCommand.java
+++ src/main/java/frc/robot/subsystems/intake/JostleCommand.java
@@ -1,12 +1,9 @@
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class JostleCommand extends Command{
     private IntakeSubsystem intakeSubsystem;
-    private final Timer timer = new Timer();
 
     /**
      * 
@@ -19,15 +16,11 @@ public class JostleCommand extends Command{
 
     @Override
     public void initialize() {
-        
     }
 
     @Override
     public void execute() {
         intakeSubsystem.runExtenderMotor(-1);
-        new WaitCommand(1);
-        intakeSubsystem.stopExtenderMotor();
-        new WaitCommand(1);
     }
 
     @Override
