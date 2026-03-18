54e22d48f954375d32b953b03020c39a47d5c77d
TheManBehindTheMemes <alex@merc-law.com>
2026-03-17 21:01:44 -0400

    Getters and setters skeleton
    I'm sorry if this is not done the way that was requested, or if it is very inefficient, or if it is just completely wrong. I'm not as competent as you may think.

---
+++ src/main/java/frc/robot/subsystems/turret/TurretMotorGettersAndSetters.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.turret;
+
+import frc.robot.subsystems.turret.TurretSubsystem;
+
+//this works kinda like the constants file
+//Feel free to delete anything here if it causes a merge conflict or error
+public class TurretMotorGettersAndSetters {
+
+    //Kicker
+    public double getKickerRPM(){
+        return 0.0; //placeholder
+    }
+
+    //spindexer
+    public double getSpindexerRPM(){
+        return 0.0; //placeholder
+    }
+
+    //spitter
+    public double getSpitterRPM(){
+        return 0.0; //placeholder
+    }
+
+    //horizontal motor
+    //not needed! (currently)
+
+    //vertical motor
+    //not needed! (currently)
+}
