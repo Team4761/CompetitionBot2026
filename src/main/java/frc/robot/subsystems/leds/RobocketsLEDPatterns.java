e7f9ceb58e791701ed60408eab2b9b6f3f4c572c
TheManBehindTheMemes <alex@merc-law.com>
2026-03-22 14:53:06 -0400

    removed DisplayLEDPatternCommand
    Turns out it was useless

--- src/main/java/frc/robot/subsystems/leds/RobocketsLEDPatterns.java
+++ src/main/java/frc/robot/subsystems/leds/RobocketsLEDPatterns.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.util.Color;
 
 /**
  * This stores all of our LED patterns.
+ * <p> note from Alex, the LED guy:
+ * <p> I am not currently using this. If I need to, I might migrate patterns from LEDSubsystem. But that's a lot of unnecassary work, considering how some of them are run.
  */
 public class RobocketsLEDPatterns {
     public static final LEDPattern OFF = LEDPattern.solid(Color.kBlack);
